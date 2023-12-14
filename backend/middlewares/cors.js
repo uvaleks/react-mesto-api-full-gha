0c6572ef5fd72668d1f204a35c328a7804fa8849
uvaleks <uvaleks@protonmail.com>
2023-12-14 22:52:25 +0300

    hotfix: api path

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -18,6 +18,7 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Credentials', true);
